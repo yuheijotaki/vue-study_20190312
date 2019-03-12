032977a7006ab8127190b9178ce55ebeedf36860
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-03-12 17:07:20 +0900

    update: myPosts.vue

--- my-app/src/components/element/posts/myPosts.vue
+++ my-app/src/components/element/posts/myPosts.vue
@@ -1,9 +1,13 @@
 <template>
   <section>
+    <div class="search_area">
+      <input type="text" v-model="tag" name="" value="">
+      <input type="button" v-on:click="filterTag()" name="" value="search">
+    </div>
     <p v-show="loading" class="loading">loading...</p>
     <ul class="post_list">
       <li v-for="(post,index) in posts" :key="index">
-        <p>{{post.updated_at}} by: <a :href="`https://qiita.com/${post.user.id}`" target="_blank">{{post.user.name}}</a></p>
+        <p>{{post.updated_at}} by: <a :href="`https://qiita.com/${post.user.id}`" target="_blank">@{{post.user.id}}</a></p>
         <h2><a :href="post.url" target="_blank">{{post.title}}</a></h2>
       </li>
     </ul>
@@ -18,16 +22,27 @@ export default {
   data () {
     return {
       posts: [],
-      loading: false
+      loading: false,
+      tag: ''
     }
   },
   created: function(){
     this.loading = true; // loading を表示する
-    this.request();
+    this.filterDefault();
   },
   methods: {
-    request: function(){
+    filterDefault: function(){
       axios.get( 'https://qiita.com/api/v2/tags/vue.js/items' )
+      .then( response => {
+        this.loading = false; // loading を非表示にする
+        this.posts = response.data;
+      })
+      .catch( error => {
+        console.log(error);
+      });
+    },
+    filterTag: function(){
+      axios.get( `https://qiita.com/api/v2/tags/${this.tag}/items` )
       .then( response => {
         this.loading = false; // loading を非表示にする
         this.posts = response.data;
@@ -44,6 +59,28 @@ export default {
 .loading {
   margin-top: 40px;
 }
+.search_area {
+  input {
+    font-size: 12px;
+    line-height: 1;
+    padding: 5px 8px;
+    box-sizing: border-box;
+    &[type="text"] {
+      background: #fff;
+      border: #ddd 1px solid;
+      color: #333;
+    }
+    &[type="button"] {
+      background: #333;
+      border: #333 1px solid;
+      color: #fff;
+      &:hover {
+        cursor: pointer;
+        opacity: .8;
+      }
+    }
+  }
+}
 .post_list {
   margin-top: 40px;
   list-style: none;
@@ -53,14 +90,17 @@ export default {
     border-bottom: 1px solid #ddd;
     line-height: 1.2;
     h2 {
-      margin-top: 5px;
-      font-size: 18px;
+      margin-top: 10px;
+      font-size: 12px;
+    }
+    p {
+      font-size: 10px;
     }
     a {
-      // color: #333;
+      color: #333;
       text-decoration: none;
+      border-bottom: #333 1px dotted;
     }
   }
 }
-
 </style>
